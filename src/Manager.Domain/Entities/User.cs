9ba712fd13b8db894ee492b5fd0312946429e458
rootslowed <rootslowed@gmail.com>
2021-09-15 14:04:50 -0300

    user entity with name, email and password properties and methods to change

---
+++ src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,44 @@
+namespace Manager.Domain.Entities
+{
+  public class User : Base
+  {
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+
+    //EF
+    protected User() { }
+
+    public User(string name, string email, string password)
+    {
+      Name = name;
+      Email = email;
+      Password = password;
+      _errors = new List<string>();
+      Validate();
+    }
+
+    public void ChangeName(string name)
+    {
+      Name = name;
+      Validate();
+    }
+
+    public void ChangePassword(string password)
+    {
+      Password = password;
+      Validate();
+    }
+
+    public void ChangeEmail(string email)
+    {
+      Email = email;
+      Validate();
+    }
+
+    public override bool Validate()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
