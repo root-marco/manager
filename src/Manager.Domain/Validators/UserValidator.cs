9283c44ae3474129bbe2669119d7117b35e53a3f
rootslowed <rootslowed@gmail.com>
2021-09-15 14:28:06 -0300

    user fields validations - not empty/null, minimum/maximum length and regex

---
+++ src/Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,35 @@
+using FluentValidation;
+using Manager.Domain.Entities;
+
+namespace Manager.Domain.Validators
+{
+  public class UserValidator : AbstractValidator<User>
+  {
+    public UserValidator()
+    {
+      RuleFor(user => user)
+        .NotEmpty().WithMessage("entity cannot be empty.")
+        .NotNull().WithMessage("entity cannot be null.");
+
+      RuleFor(user => user.Name)
+        .NotEmpty().WithMessage("name cannot be empty.")
+        .NotNull().WithMessage("name cannot be null.")
+        .MinimumLength(3).WithMessage("minimum characters for NAME: 3")
+        .MaximumLength(80).WithMessage("maximum characters for NAME: 80");
+
+      RuleFor(user => user.Password)
+        .NotEmpty().WithMessage("password cannot be empty.")
+        .NotNull().WithMessage("passoword cannot be null.")
+        .MinimumLength(6).WithMessage("minimum characters for PASSSWORD: 6")
+        .MaximumLength(80).WithMessage("maximum characters for PASSSWORD: 80");
+
+      RuleFor(user => user.Email)
+        .NotEmpty().WithMessage("name cannot be empty.")
+        .NotNull().WithMessage("email cannot be null.")
+        .MinimumLength(10).WithMessage("minimum characters for EMAIL: 10")
+        .MaximumLength(180).WithMessage("maximum characters for EMAIL: 180")
+        .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+        .WithMessage("email invalid");
+    }
+  }
+}
