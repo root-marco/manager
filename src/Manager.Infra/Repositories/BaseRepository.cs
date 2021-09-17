15e4e34675018c9d46f07781c4b3e4f921a8611d
rootslowed <rootslowed@gmail.com>
2021-09-16 23:04:21 -0300

    base repository for all repos

---
+++ src/Manager.Infra/Repositories/BaseRepository.cs
@@ -0,0 +1,36 @@
+namespace Manager.Infra.Repositories
+{
+  public class BaseRepository<T> : IBaseRepository<T> where T : Base
+  {
+    private readonly ManagerContext _context;
+
+    public BaseRepository(ManagerContext context)
+    {
+      _context = context;
+    }
+
+    public virtual async Task<T> Create(T obj)
+    {
+      _context.Add(obj);
+      await _context.SaveChangesAsync();
+      return obj;
+    }
+
+    public virtual async Task<T> Update(T obj)
+    {
+      _context.Entry(obj).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
+
+      return obj;
+    }
+
+    public virtual async Task<T> Remove(long id) {
+      var obj = await Get(id);
+
+      if (obj != null) {
+        _context.Remove(obj);
+        await _context.SaveChangesAsync();
+      }
+    }
+  }
+}
