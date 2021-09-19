aad5db621ff5a3fbfc19809e6eaadc26f3737d50
root-marco <rootslowed@gmail.com>
2021-09-19 15:55:00 -0300

    user repository implemented to save the users in database

--- src/4-Manager.Infra/Repositories/UserRepository.cs
+++ src/4-Manager.Infra/Repositories/UserRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,9 +20,8 @@ namespace Manager.Infra.Repositories
 
     public async Task<User> GetByEmail(string email)
     {
-      var user = await _context
-        .Users
-        .Where(x => x.Email.ToLower() == email.ToLower())
+      var user = await _context.Users
+        .Where(x => string.Equals(x.Email, email, StringComparison.CurrentCultureIgnoreCase))
         .AsNoTracking()
         .ToListAsync();
 
@@ -30,22 +30,20 @@ namespace Manager.Infra.Repositories
 
     public async Task<List<User>> SearchByEmail(string email)
     {
-      var allUsers = await _context
-      .Users
-      .Where(x => x.Email.ToLower().Contains(email.ToLower()))
-      .AsNoTracking()
-      .ToListAsync();
+      var allUsers = await _context.Users
+        .Where(x => x.Email.ToLower().Contains(email.ToLower()))
+        .AsNoTracking()
+        .ToListAsync();
 
       return allUsers;
     }
 
     public async Task<List<User>> SearchByName(string name)
     {
-      var allUsers = await _context
-      .Users
-      .Where(x => x.Name.ToLower().Contains(name.ToLower()))
-      .AsNoTracking()
-      .ToListAsync();
+      var allUsers = await _context.Users
+        .Where(x => x.Name.ToLower().Contains(name.ToLower()))
+        .AsNoTracking()
+        .ToListAsync();
 
       return allUsers;
     }
