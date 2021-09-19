0b4099bb817cb464d32e8a8376ed942564159214
root-marco <rootslowed@gmail.com>
2021-09-19 15:54:26 -0300

    interface - user services with CRUD and search

---
+++ src/3-Manager.Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Manager.Services.Dtos;
+
+namespace Manager.Services.Interfaces
+{
+  public interface IUserService
+  {
+    Task<UserDto> Create(UserDto userDto);
+    Task<UserDto> Update(UserDto userDto);
+    Task Remove(long id);
+    Task<List<UserDto>> Get();
+    Task<List<UserDto>> SearchByName(string name);
+    Task<List<UserDto>> SearchByEmail(string email);
+    Task<UserDto> GetByEmail(string email);
+  }
+}
