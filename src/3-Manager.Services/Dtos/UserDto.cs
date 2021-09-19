18dbfa5660d579dd1079a576915ad75dcca1ec68
root-marco <rootslowed@gmail.com>
2021-09-19 15:57:39 -0300

    user dto done

---
+++ src/3-Manager.Services/Dtos/UserDto.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+
+namespace Manager.Services.Dtos
+{
+  public class UserDto
+  {
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    [JsonIgnore] public string Password { get; set; }
+
+    public UserDto() {
+    }
+
+    public UserDto(long id, string name, string email, string password)
+    {
+      Id = id;
+      Name = name;
+      Email = email;
+      Password = password;
+    }
+  }
+}
