53ef50b9cf813da7b31cd032bcdcf2852dd7f699
root-marco <rootslowed@gmail.com>
2021-09-19 15:57:25 -0300

    context database - entity framework core

--- src/4-Manager.Infra/Context/ManagerContext.cs
+++ src/4-Manager.Infra/Context/ManagerContext.cs
@@ -1,3 +1,5 @@
+using Manager.Domain.Entities;
+using Manager.Infra.Mappings;
 using Microsoft.EntityFrameworkCore;
 
 namespace Manager.Infra.Context
@@ -6,7 +8,22 @@ namespace Manager.Infra.Context
   {
     public ManagerContext()
     {
+    }
 
+    public ManagerContext(DbContextOptions<ManagerContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      const string uri = @"Server=localhost;Database=USER_MANAGER;User Id=SA;Password=xmm$tzb$J61;";
+      optionsBuilder.UseSqlServer(uri);
+    }
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.ApplyConfiguration(new UserMap());
     }
   }
 }
