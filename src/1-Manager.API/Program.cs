78e2a01e4028f7dd8c193329899490faea5d2364
root-marco <rootslowed@gmail.com>
2021-09-20 11:16:51 -0300

    indentation fixed

--- src/1-Manager.API/Program.cs
+++ src/1-Manager.API/Program.cs
@@ -17,9 +17,9 @@ namespace Manager.API
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+      Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
   }
 }
