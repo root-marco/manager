980e9a7d401ce8567d97199b9b9089b8f851d738
root-marco <rootslowed@gmail.com>
2021-09-20 11:17:46 -0300

    user dto small changes

--- src/3-Manager.Services/Dtos/UserDto.cs
+++ src/3-Manager.Services/Dtos/UserDto.cs
@@ -7,7 +7,6 @@ namespace Manager.Services.Dtos
     public long Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-
     [JsonIgnore] public string Password { get; set; }
 
     public UserDto() {
