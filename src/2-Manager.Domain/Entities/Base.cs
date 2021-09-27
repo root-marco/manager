61b4ccba49829869a2b30cc95530541f7c11dceb
root-marco <rootslowed@gmail.com>
2021-09-27 15:22:23 -0300

    base entity

--- src/2-Manager.Domain/Entities/Base.cs
+++ src/2-Manager.Domain/Entities/Base.cs
@@ -2,11 +2,11 @@ using System.Collections.Generic;
 
 namespace Manager.Domain.Entities
 {
-	public abstract class Base
-	{
+  public abstract class Base
+  {
     public long Id { get; set; }
     internal List<string> _errors;
     public IReadOnlyCollection<string> Errors => _errors;
     public abstract bool Validate();
-	}
+  }
 }
