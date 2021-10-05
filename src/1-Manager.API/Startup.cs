bdfc650e0968fac14385a811d7380bf24178ebaf
root-marco <rootslowed@gmail.com>
2021-10-05 14:05:09 -0300

    JWT and SWAGGER configs implemented

--- src/1-Manager.API/Startup.cs
+++ src/1-Manager.API/Startup.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Text;
 using AutoMapper;
+using Manager.API.Tokens;
 using Manager.API.ViewModels;
 using Manager.Domain.Entities;
 using Manager.Infra.Context;
@@ -7,19 +10,20 @@ using Manager.Infra.Repositories;
 using Manager.Services.Dtos;
 using Manager.Services.Interfaces;
 using Manager.Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Manager.API
 {
   public class Startup
   {
-    public Startup(IConfiguration configuration)
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
       Configuration = configuration;
     }
@@ -46,32 +50,105 @@ namespace Manager.API
       #region Dependencies Injection
 
       services.AddSingleton(d => Configuration);
-      services.AddDbContext<ManagerContext>(
-        options => options.UseSqlServer(Configuration["ConnectionStrings:USER_MANAGER"]), ServiceLifetime.Transient);
       services.AddScoped<IUserService, UserService>();
       services.AddScoped<IUserRepository, UserRepository>();
+      services.AddScoped<ITokenGenerator, TokenGenerator>();
+
+      services.AddDbContext<ManagerContext>(options =>
+      {
+        var connectionString = Configuration.GetConnectionString("USER_MANAGER");
+
+        options.UseSqlServer(connectionString);
+      });
 
       #endregion
 
-      services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Manager.API", Version = "v1"}); });
+      #region JWT
+
+      var secretKey = Configuration["Jwt:Key"];
+
+      services.AddAuthentication(x =>
+      {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer(x =>
+      {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
+          ValidateIssuer = false,
+          ValidateAudience = false
+        };
+      });
+
+      #endregion
+
+      # region SWAGGER
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "Manager API",
+          Version = "v1",
+          Description = "API construída na serie de vídeos no canal Lucas Eschechola.",
+          Contact = new OpenApiContact
+          {
+            Name = "Marco Antônio",
+            Email = "rootslowed@gmail.com",
+            Url = new Uri("https://eschechola.com.br")
+          },
+        });
+
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+          In = ParameterLocation.Header,
+          Description = "Missing Bearer <TOKEN>",
+          Name = "Authorization",
+          Type = SecuritySchemeType.ApiKey
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+            {
+              Type = ReferenceType.SecurityScheme,
+              Id = "Bearer"
+            }
+          },
+          new string[] { }
+        }
+      });
+      });
+
+      # endregion
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Manager.API v1"));
-      }
+      app.UseDeveloperExceptionPage();
+
+      app.UseSwagger();
+
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Manager.API v1"));
 
       app.UseHttpsRedirection();
 
       app.UseRouting();
 
+      app.UseAuthentication();
+
       app.UseAuthorization();
 
-      app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
     }
   }
 }
