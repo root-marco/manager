0a27c1017177edb9bb8a3e606ab5fae57792957b
root-marco <rootslowed@gmail.com>
2021-10-05 14:05:29 -0300

    manager context to handle sessions on database

--- src/4-Manager.Infra/Context/ManagerContext.cs
+++ src/4-Manager.Infra/Context/ManagerContext.cs
@@ -6,19 +6,11 @@ namespace Manager.Infra.Context
 {
   public class ManagerContext : DbContext
   {
-    public ManagerContext()
-    {
-    }
-
-    public ManagerContext(DbContextOptions<ManagerContext> options) : base(options)
+    public ManagerContext(DbContextOptions<ManagerContext> options)
+      : base(options)
     {
     }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-      builder.ApplyConfiguration(new UserMap());
-    }
   }
 }
