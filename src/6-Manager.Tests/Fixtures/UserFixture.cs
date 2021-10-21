0eaa08dc1141431e2cf13b6e8a6e35c76cac074c
root-marco <rootslowed@gmail.com>
2021-10-21 08:56:48 -0300

    user fixture to create valid and invalid users

---
+++ src/6-Manager.Tests/Fixtures/UserFixture.cs
@@ -0,0 +1,53 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Manager.Domain.Entities;
+using Manager.Services.Dtos;
+using System.Collections.Generic;
+
+namespace Manager.Tests.Fixtures
+{
+  public class UserFixture
+  {
+    public static User CreateValidUser()
+    {
+      return new User(
+        name: new Name().FirstName(),
+        email: new Internet().Email(),
+        password: new Internet().Password());
+    }
+
+    public static List<User> CreateListValidUser(int limit = 5)
+    {
+      var list = new List<User>();
+
+      for (int i = 0; i < limit; i++)
+      {
+        list.Add(CreateValidUser());
+      }
+
+      return list;
+    }
+
+    public static UserDto CreateValidUserDto(bool newId = false)
+    {
+      return new UserDto
+      {
+        Id = newId ? new Randomizer().Int(0, 1000) : 0,
+        Name = new Name().FirstName(),
+        Email = new Internet().Email(),
+        Password = new Internet().Password()
+      };
+    }
+
+    public static UserDto CreateInvalidUserDto()
+    {
+      return new UserDto
+      {
+        Id = 0,
+        Name = "",
+        Email = "",
+        Password = ""
+      };
+    }
+  }
+}
